7d0d1eee4c586b67d0422d41eee83c7b154ce395
Abhi Singh <uchihamadarat99@gmail.com>
2025-04-14 10:50:01 +0530

    Day-2 of my project on: 4/14/2025(10:49)

---
+++ practices_byDays/day_2.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+// This code prints "Hello, World!" to the console and returns 0 to indicate successful completion. It uses the C standard library for input/output operations.
