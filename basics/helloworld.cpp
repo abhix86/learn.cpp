58ddf0e00c672a7141828c8e03b80c90ce3eb28f
Abhi Singh <uchihamadarat99@gmail.com>
2025-04-14 20:32:51 +0530

    Hello,World! program

--- basics/helloworld.cpp
+++ basics/helloworld.cpp
@@ -1,7 +1,15 @@
 #include <iostream> // Include the iostream library for input and output operations
 
 int main() { 
-    std::cout << "Hello, World!\n"; 
-    std::cout << "Howdy?";
+    std::cout << "Hello, World!" << std::endl; 
+    std::cout << "Howdy?" << '\n';
+    std::cout << "I'm good, thanks for asking\n";
+    /*
+        So, in this program we are using the iostream library to print "Hello, World!" to the console.
+        The std::cout is used to output data to the standard output stream (usually the console).
+        The std::endl is used to insert a new line and flush the output buffer.
+        The '\n' is another way to insert a new line.   
+        The other way to insert a new line is to add `\n` to the end of the string.
+    */
     return 0; 
 }
