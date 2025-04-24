6b9cc71356f9d9bcb3a72c67fc5cc432df150ce8
Abhi Singh <uchihamadarat99@gmail.com>
2025-04-24 21:14:50 +0530

    consists for loop, pattern printing program

--- basics/loops.cpp
+++ basics/loops.cpp
@@ -57,6 +57,7 @@ int main() {
         }
     cout<<endl;
     }
+    // program 7: idk what to name this, maybe diamond container... ?
     cout<<"\nProgram 7: \n";
     for(int i = 1; i <= 5; i++){
         for(int j =1; j <=6 - i; j++){
